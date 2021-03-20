392f7ade3143dc75ff32e7f0167a835a99143d68
Eka Juliawati <ekajuliawati63@gmail.com>
2021-03-20 12:01:47 +0700

    perulanganfor dart

--- dasar_dart/bin/perulanganfor.dart
+++ dasar_dart/bin/perulanganfor.dart
@@ -1,7 +1,7 @@
 import 'package:dasar_dart/dasar_dart.dart' as dasar_dart;
 import 'dart:io';
 
-main(List<String> args) {
+void main(List<String> args) {
   for (int counter = 0; counter < 10; counter = counter + 1) {
     print("hallo");
   }
