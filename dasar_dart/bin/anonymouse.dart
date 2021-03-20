22f990d3c42380b8d9e5441ff7c73740bc55ab96
Eka Juliawati <ekajuliawati63@gmail.com>
2021-03-20 12:03:09 +0700

    anonymouse dart

---
+++ dasar_dart/bin/anonymouse.dart
@@ -0,0 +1,28 @@
+import 'package:dasar_dart/dasar_dart.dart' as dasar_dart;
+import 'dart:io';
+
+String say(String from, String message, {String to, String appName}) {
+  return from +
+      " say " +
+      ((to != null) ? " to " + to : "") +
+      ((appName != null) ? " Via " + appName : "");
+}
+
+String say2(String from, String message,
+    {String to, String appName = "Whatshapp"}) {
+  return from +
+      " say " +
+      ((to != null) ? " to " + to : "") +
+      ((appName != null) ? " Via " + appName : "");
+}
+
+double luas_segiempat(double panjang, double lebar) => panjang * lebar;
+int doMathOperator(int number1, int number2, Function operator) {
+  return operator(number1, number2);
+}
+
+void main(List<String> args) {
+  Function f;
+  f = luas_segiempat;
+  print(say("Johny", " Hello", to: "Eka", appName: "whatshapp"));
+}
