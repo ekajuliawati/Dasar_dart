adcf46061b8e74f7b2d102a30beff3946c8f1d56
Eka Juliawati <ekajuliawati63@gmail.com>
2021-03-20 09:46:33 +0700

    prulangan while

---
+++ dasar_dart/bin/perulanganwhile.dart
@@ -0,0 +1,17 @@
+import 'package:dasar_dart/dasar_dart.dart' as dasar_dart;
+import 'dart:io';
+
+main(List<String> args) {
+  int i = 0;
+  while (i < 5) {
+    print("halo ke " + i.toString());
+    i += 1;
+  }
+  print("============");
+
+  int x = 0;
+  do {
+    print("dododo ke " + x.toString());
+    x += 1;
+  } while (x < 5);
+}
