5ecaaa2f16711aae4acba4fc0973fc21a260cd08
Eka Juliawati <ekajuliawati63@gmail.com>
2021-03-20 10:09:47 +0700

    fungsi optional

---
+++ dasar_dart/bin/optionalparameter.dart
@@ -0,0 +1,20 @@
+import 'package:dasar_dart/dasar_dart.dart' as dasar_dart;
+import 'dart:io';
+
+String say(String from, String message, {String to, String appName}) {
+  return from +
+      " say " +
+      ((to != null) ? " to " + to : "") +
+      ((appName != null) ? " Via " + appName : "");
+}
+
+String say2(String from, String message, {String to, String appName}) {
+  return from +
+      " say " +
+      ((to != null) ? " to " + to : "") +
+      ((appName != null) ? " Via " + appName : "");
+}
+
+main(List<String> args) {
+  print(say("johny", " Hello", to: "Abidah", appName: "whatshapp"));
+}
